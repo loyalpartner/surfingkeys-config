06f8df38d25e361f9c7f576a9e47e3a35f8ad44f
lee <loyalpartner@163.com>
2020-07-01 17:16:51 +0800

    build result.js

--- docs/result.js
+++ docs/result.js
@@ -149,29 +149,6 @@ mapkey("<Space>a", "Capture", ()=>{
 });
 
 ///////////////////////////////////////////////////////////////////////////////
-// baidu-translate
-var appid = '20200607000488675';
-var key = 'Nb_cT61hFraVEUpkvp33'
-
-const translate_show_result = (res) =>{
-    var json = JSON.parse(res.text)
-    Front.showPopup(json.trans_result.map((t)=> `<p>${t.src}</p><p>${t.dst}<p>`).join("\n"))
-}
-
-const translate_handle_query = (text)=> text.replace(/\n/g, " ").replace(/([^.]{3})\.\s+/g, "\1.\n")
-
-const translate_text = (text) => {
-    text = translate_handle_query(text)
-    var salt = '3329757864'
-    var str1 = appid + text + salt +key;
-    var sign = MD5(str1);
-    var url =
-        `http://api.fanyi.baidu.com/api/trans/vip/translate?from=auto&to=zh&appid=${appid}&sign=${sign}&q=${encodeURI(text)}&salt=${salt}`
-    httpRequest({url: url} , translate_show_result)
-}
-
-mapkey('<Space>yy', 'baidu translate', ()=> {Hints.create('h1,h2,h3,h4,h5,h6,p,li', (e)=> translate_text(e.textContent))});
-///////////////////////////////////////////////////////////////////////////////
 // inline query
 const parse_translate_result = (res) => {
     res = JSON.parse(res.text);
@@ -216,6 +193,29 @@ settings.theme = `
 #sk_status, #sk_find { font-size: 20pt; }`;
 // click `Save` button to make above settings to take effect.
 // click `Save` button to make above settings to take effect.
+///////////////////////////////////////////////////////////////////////////////
+// baidu-translate
+var appid = '20200607000488675';
+var key = 'Nb_cT61hFraVEUpkvp33'
+
+const translate_show_result = (res) =>{
+    var json = JSON.parse(res.text)
+    Front.showPopup(json.trans_result.map((t)=> `<p>${t.src}</p><p>${t.dst}<p>`).join("\n"))
+}
+
+const translate_handle_query = (text)=> text.replace(/\n/g, " ").replace(/([^.]{3})\.\s+/g, "\1.\n")
+
+const translate_text = (text) => {
+    text = translate_handle_query(text)
+    var salt = '3329757864'
+    var str1 = appid + text + salt +key;
+    var sign = MD5(str1);
+    var url =
+        `http://api.fanyi.baidu.com/api/trans/vip/translate?from=auto&to=zh&appid=${appid}&sign=${sign}&q=${encodeURI(text)}&salt=${salt}`
+    httpRequest({url: url} , translate_show_result)
+}
+
+mapkey('<Space>yy', 'baidu translate', ()=> {Hints.create('h1,h2,h3,h4,h5,h6,p,li', (e)=> translate_text(e.textContent))});
 var MD5 = function (string) {
   
     function RotateLeft(lValue, iShiftBits) {
